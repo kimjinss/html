f72b4e119ec23a53fc832edbb42a3a2623bde8cb
kimjinss <77815816+kimjinss@users.noreply.github.com>
2021-02-15 09:41:38 +0900

    routes

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const {spawn} = require('child_process');
+const { PythonShell } = require("python-shell");
+const http = require('http');
+dotenv.config();
 const app = express();
+const indexRouter = require('./routes')
 app.set('port', process.env.PORT || 8080);
 
-app.get('/', (req, res) => {
-    // res.send('Hello, Express');
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
+// app.get('/', (req, res) => {
+//     // res.send('Hello, Express');
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// });
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
+
+app.use(function(req, res, next) {
+
+    res.send({name:'test', age:'20'});
+});
+
+
