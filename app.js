184bf6fd02e5a42edeb7b12072b789fa66a3104f
kimjinss <77815816+kimjinss@users.noreply.github.com>
2021-02-15 10:29:56 +0900

    error

--- app.js
+++ app.js
@@ -12,13 +12,24 @@ app.set('port', process.env.PORT || 8080);
 //     res.sendFile(path.join(__dirname, '/index.html'));
 // });
 
+
+app.use('/',indexRouter)
+
+
+
+
+
+
+
+app.use((req,res,next) => {
+    res.status(404).send('Not Found');
+});
+
+app.use((err,req,res,next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
-app.use('/',indexRouter)
-// app.use(function(req, res, next) {
-//
-//     res.send({name:'test', age:'20'});
-// });
-
 
