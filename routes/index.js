2d0af111612f7ee585b2d515aaee47548c2933d5
kimjinss <77815816+kimjinss@users.noreply.github.com>
2021-02-17 17:12:30 +0900

    test9

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ const client = new Client({
     database: 'test1db',
     password: 'test1',
     port: '5432',
+    max: 10, // max number of clients in the pool
+
+    idleTimeoutMillis: 30000, // how long a client
+
 });
 
 
